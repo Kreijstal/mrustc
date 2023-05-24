f38ef3e521fa1d40bbdcae3b4db08d52dcad1683
Ookiineko <chiisaineko@protonmail.com>
2023-05-24 11:18:15 +0800

    build: use more jobs for building mrustc
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- build-1.19.0.sh
+++ build-1.19.0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 export RUSTC_VERSION=1.19.0 MRUSTC_TARGET_VER=1.19 OUTDIR_SUF=-1.19.0
-make
+make -j $(($(nproc) - 1))
 make -f minicargo.mk RUSTCSRC
 make -f minicargo.mk LIBS
 make -f minicargo.mk test
